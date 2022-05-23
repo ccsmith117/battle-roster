2cefa3eba01ddfc33b37dbd4c3c16524f4cf990a
csmith <cody.charles.smith@gmail.com>
2022-05-22 20:41:08 -0400

    Adds Prettier to project, applies Prettier styling to entire project, guts App.tsx to display a basic intro page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  text-align: center;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <h2>Battle Roster</h2>
+      <span>
+        Welcome to the beginning of Battle Roster, a statistics app to
+        beautifully and intuitively display information about the competitive
+        Warmachine / Hordes scene.
+      </span>
+      <br />
+      <span>
+        Whether you want to see the results of the latest tournaments or find
+        inspiration for your next list to win your next tournament, Battle
+        Roster hopes to make doing that an enjoyable experience.
+      </span>
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
